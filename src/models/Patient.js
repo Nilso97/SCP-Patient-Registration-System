5a77d3edce05c6585aa6db39af8c48245bfd2a09
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2022-09-10 18:03:28 -0300

    Schemas do MongoDB

--- src/models/Patient.js
+++ src/models/Patient.js
@@ -4,24 +4,60 @@ const mongoose = require("mongoose");
 MongoDB Schemas
 */
 const dataSchema = new mongoose.Schema({
-  patient_name: {
-    required: true,
-    type: String,
+  patient_data: {
+    patient_name: {
+      required: true,
+      type: String,
+    },
+  
+    birth_date: {
+      required: true,
+      type: String,
+    },
+  
+    sex: {
+      required: true,
+      type: String,
+    },
+  
+    email: {
+      required: true,
+      type: String,
+    },
+  
+    address: {
+      required: true,
+      type: String,
+    },
   },
 
-  birth_date: {
-    required: true,
-    type: String,
+  information: {
+    symptons: {
+      required: true,
+      type: String,
+    },
+
+    health_insurance: {
+      required: true,
+      type: String,
+    },
+
+    more_information: {
+      required: false,
+      type: String,
+    },
   },
 
-  email: {
-    required: true,
-    type: String,
-  },
+  schedule_appointment: {
+    consultation_date: {
+      required: true,
+      type: String,
+    },
 
-  address: {
-    required: true,
-    type: String,
+    appointment_time: {
+      required: true,
+      type: String,
+    },
   },
 });
 
